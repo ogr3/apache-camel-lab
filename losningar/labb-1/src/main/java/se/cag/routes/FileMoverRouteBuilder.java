b888dc8bfe709e903c5486794212b8f35b020b73
benny <benny.henriksson@cag.se>
2017-11-21 12:25:30 +0100

    Lagt rätt versioner av labbarna i modulen losningar samt skapat modulen ovningar.

---
+++ losningar/labb-1/src/main/java/se/cag/routes/FileMoverRouteBuilder.java
@@ -0,0 +1,15 @@
+package se.cag.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import se.cag.routes.processor.UppercaseProcessor;
+
+public class FileMoverRouteBuilder extends RouteBuilder {
+//
+    @Override
+    public void configure() throws Exception {
+        from("file://target/inbox")
+//            lägg in processorn UppercaseProcessor
+            .process(new UppercaseProcessor())
+            .to("file://target/outbox");
+    }
+}
