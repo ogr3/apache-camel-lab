81bb21cf1daef8544a15a385294d86e470edcb57
benny <benny.henriksson@cag.se>
2017-11-21 15:02:05 +0100

    Lagt labb-1 modulen ovningar och döpt om labb-1 i losningar (losning-labb-1) för att förhindra strul med maven.

---
+++ ovningar/labb-1/src/main/java/se/cag/routes/processor/UppercaseProcessor.java
@@ -0,0 +1,15 @@
+package se.cag.routes.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class UppercaseProcessor implements Processor{
+
+    public void process(Exchange exchange) throws Exception {
+//        Hämta ut filens innehåll body-delen av exchange
+
+//        Gör om innehållet till stora bokstäver
+
+//        Ersätt innehållet i body med det nya
+    }
+}
