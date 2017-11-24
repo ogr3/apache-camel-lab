a592dc9eb9209b1eb949ecf4965b50b53b5a80f2
oGre <ogre@muppfarmen.se>
2017-11-24 10:13:07 +0100

    Test

--- camel-demo/src/test/java/se/cag/routes/ProcessorRouteBuilderTest.java
+++ camel-demo/src/test/java/se/cag/routes/ProcessorRouteBuilderTest.java
@@ -1,26 +1,36 @@
 package se.cag.routes;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.builder.NotifyBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
+import java.io.File;
+
 public class ProcessorRouteBuilderTest extends CamelTestSupport {
 
-  protected RouteBuilder createRouteBuilder() {
-    return new ProcessorRouteBuilder();
-  }
+    protected RouteBuilder createRouteBuilder() {
+        return new ProcessorRouteBuilder();
+    }
 
-  public void setUp() throws Exception {
-    deleteDirectory("target/inbox");
-    deleteDirectory("target/outbox");
-    super.setUp();
-  }
+    public void setUp() throws Exception {
+        deleteDirectory("target/inbox");
+        deleteDirectory("target/outbox");
+        super.setUp();
+    }
 
-  @Test
-  public void testMoveFile() throws Exception {
-    // ProducerTemplate can send messages to routes
-    template.sendBodyAndHeader(
-        "file://target/inbox", "Hello World", Exchange.FILE_NAME, "hello.txt");
-  }
+    @Test
+    public void testMoveFile() throws Exception {
+        NotifyBuilder notifyBuilder = new NotifyBuilder(context).whenDone(1).create();
+
+        // ProducerTemplate can send messages to routes
+        template.sendBodyAndHeader(
+                "file://target/inbox", "Hello World", Exchange.FILE_NAME, "hello.txt");
+        assertTrue(notifyBuilder.matchesMockWaitTime());
+        File file = new File("target/outbox/hello.txt");
+        assertTrue(file.exists());
+        String s = context.getTypeConverter().convertTo(String.class, file);
+        assertTrue("Hello World".equals(s));
+    }
 }
